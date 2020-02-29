9e7a9a11c8a2f86dbf76da95e08af62b06af5a8e
Christian Wolfram <c.wolfram@chriwo.de>
2020-02-29 21:09:41 +0100

    [TASK] Set compatibility for TYPO3 9.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.1.0-dev',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-8.7.99'
+            'typo3' => '8.7.0-9.5.99'
         ],
         'conflicts' => [
         ],
