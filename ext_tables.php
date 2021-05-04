ebb0c04b6081a1ea4edf474f6740f04616621b21
Christian Wolfram <c.wolfram@chriwo.de>
2021-05-04 10:29:55 +0200

    [TASK] Refactor and disallow records on standard pages

--- ext_tables.php
+++ ext_tables.php
@@ -1,18 +1,13 @@
 <?php
 defined('TYPO3_MODE') || die();
 
-$boot = function () {
-    // CSH - context sensitive help
-    foreach (['mail'] as $table) {
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages(
-            'tx_mailhandler_domain_model_' . $table
-        );
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
-            'tx_mailhandler_domain_model_' . $table,
-            'EXT:mailhandler/Resources/Private/Language/locallang_csh_' . $table . '.xlf'
-        );
-    }
-};
-
-$boot();
-unset($boot);
+(function () {
+   if (TYPO3_MODE === 'BE') {
+        foreach (['mail'] as $table) {
+            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
+                'tx_mailhandler_domain_model_' . $table,
+                'EXT:mailhandler/Resources/Private/Language/locallang_csh_' . $table . '.xlf'
+            );
+        }
+   }
+})();
