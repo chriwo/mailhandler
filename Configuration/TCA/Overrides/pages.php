8c2a2d7f74f5c03f540e5eaa50181da2d00f9eb8
Christian Wolfram <c.wolfram@chriwo.de>
2021-05-04 10:55:33 +0200

    [TASK] Use another page tree icon if module "mailhandler" is set

---
+++ Configuration/TCA/Overrides/pages.php
@@ -0,0 +1,10 @@
+<?php
+defined('TYPO3_MODE') || die();
+
+$GLOBALS['TCA']['pages']['columns']['module']['config']['items'][] = [
+    0 => 'LLL:EXT:mailhandler/Resources/Private/Language/locallang_be.xlf:mailhandler-folder',
+    1 => 'mailhandler',
+    2 => 'pagetree-folder-contains-mailhandler'
+];
+
+$GLOBALS['TCA']['pages']['ctrl']['typeicon_classes']['contains-mailhandler'] = 'pagetree-folder-contains-mailhandler';
