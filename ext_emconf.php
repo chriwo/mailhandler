e887e6466963e059949d1ab558531a1b274d2a7d
Christian Wolfram <c.wolfram@chriwo.de>
2021-05-04 08:27:24 +0200

    [TASK] Set TYPO3 CMS 10.4 as minimal requirement

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '2.0.0-dev',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-9.5.99'
+            'typo3' => '10.4.0-10.4.99'
         ],
         'conflicts' => [
         ],
