2352ae621e1649d7b8bf0c2d83a8dae81edf499b
Christian Wolfram <c.wolfram@chriwo.de>
2022-08-17 09:19:25 +0200

    Revert "[!!!][TASK] Set requirement to TYPO3 CMS v11.5"
    This reverts commit 04edc6a6dac8d7850f6832e867d55d27cea19a9b.

---
+++ ext_emconf.php
@@ -0,0 +1,24 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Mail handler',
+    'description' => 'Easy way to create email templates and use in other TYPO3 extensions',
+    'category' => 'module',
+    'author' => 'Christian Wolfram',
+    'author_email' => 'c.wolfram@chriwo.de',
+    'author_company' => '',
+    'state' => 'beta',
+    'uploadfolder' => false,
+    'modify_tables' => '',
+    'clearCacheOnLoad' => true,
+    'version' => '2.1.0-dev',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '10.4.0-10.4.99'
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+];
